5f4f807b2a8a7ce4145799e98c7d8b5f83192b74
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2023-09-07 17:13:30 +1200

    fix: fixed lefthook

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-class a {
-  constructor() {}
-}
-
 const root = document.getElementById('td-root') as HTMLElement;
 
 createRoot(root).render(
